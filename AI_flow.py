27386a0ede15455bf92129eadf9e5294872fec2b
Evelyn Voce <74924781+EvelynVoce@users.noreply.github.com>
2022-01-11 17:39:00 +0000

    Fixed the bug where name extraction wasn't working

--- AI_flow.py
+++ AI_flow.py
@@ -13,7 +13,7 @@ kb: list = [read_expr(row.lower()) for row in data[0]]
 
 # Checking KB integrity (no contradiction), otherwise show an error message and terminate
 for knowledge in kb:
-    if not ResolutionProver().prove(knowledge, kb, verbose=True):
+    if not ResolutionProver().prove(knowledge, kb):
         print("ERROR: CONTRADICTION FOUND")
         quit()
 
@@ -47,12 +47,14 @@ def get_ai_response(kern, user_input: str) -> str:
             output = "Sorry, I do not know that. Be more specific!"
 
     elif cmd == '3':  # Memory triggers
+        print(user_input)
         if "my name is" in user_input.lower():  # Extract name
             global user_name
             user_name = extract_name(user_input)
-            print(user_name)
+            print("Name = ", user_name)
             if user_name is None:
                 user_name = ""
+            return f"Hello {user_name}, it's really nice to meet you"
 
     elif cmd == '4':  # Memory retrieval
         if "my name" in user_input.lower():
@@ -67,12 +69,12 @@ def get_ai_response(kern, user_input: str) -> str:
         # Make sure expr does not contradict
         # with the KB before appending, otherwise show an error message.
 
-        answer = ResolutionProver().prove(expr, kb, verbose=True)
+        answer = ResolutionProver().prove(expr, kb)
         if not answer:
             print("Wait one second")
 
             expr2 = read_expr("-" + object2 + '(' + object1 + ')')
-            proven = ResolutionProver().prove(expr2, kb, verbose=True)
+            proven = ResolutionProver().prove(expr2, kb)
             if proven:
                 return "ERROR: CONTRADICTION FOUND"
 
@@ -82,12 +84,12 @@ def get_ai_response(kern, user_input: str) -> str:
     elif cmd == "32":  # Check that x is y
         object1, object2 = output.split(' is ')
         expr = read_expr(object2 + '(' + object1 + ')')
-        answer = ResolutionProver().prove(expr, kb, verbose=True)
+        answer = ResolutionProver().prove(expr, kb)
         if answer:
             return f"I know that {object1} is {object2}"
         else:
             expr = read_expr("-" + object2 + '(' + object1 + ')')
-            proven = ResolutionProver().prove(expr, kb, verbose=True)
+            proven = ResolutionProver().prove(expr, kb)
             return "That is false"if proven else "I am unable to confirm that statement"
 
     elif cmd == "33":  # Fuzzy logic test
--- fuzzy.py
+++ fuzzy.py
@@ -17,18 +17,19 @@ def fuzzy_logic(writing_score: int, acting_score: int, impact_score: int):
     fs.add_linguistic_variable("Impact", LinguisticVariable(func(), universe_of_discourse=[0, 10]))
 
     # Define output fuzzy sets and linguistic variable
-    q_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=3), term="poor")
-    q_2 = FuzzySet(function=Triangular_MF(a=0, b=3, c=6), term="average")
-    q_3 = FuzzySet(function=Triangular_MF(a=3, b=6, c=9), term="good")
-    q_4 = FuzzySet(function=Trapezoidal_MF(a=6, b=9, c=10, d=10), term="amazing")
+    q_1 = FuzzySet(function=Triangular_MF(a=0, b=0, c=2.5), term="poor")
+    q_2 = FuzzySet(function=Triangular_MF(a=0, b=2.5, c=5), term="average")
+    q_3 = FuzzySet(function=Triangular_MF(a=2.5, b=5, c=7.5), term="good")
+    q_4 = FuzzySet(function=Trapezoidal_MF(a=5, b=7.5, c=10, d=10), term="amazing")
     fs.add_linguistic_variable("Quality", LinguisticVariable([q_1, q_2, q_3, q_4], universe_of_discourse=[0, 10]))
 
-    R1 = "IF (Writing IS poor) OR (Acting IS poor) THEN (Quality IS poor)"
-    R2 = "IF (Writing IS good) AND (Acting IS average) THEN (Quality IS average)"
-    R3 = "IF (Acting IS good) AND (Writing IS average) THEN (Quality IS average)"
-    R4 = "IF (Writing IS good) AND (Acting IS good) THEN (Quality IS good)"
-    R5 = "IF (Writing IS good) AND (Acting IS good) AND (Impact IS good) THEN (Quality IS amazing)"
-    fs.add_rules([R1, R2, R3, R4, R5])
+    R1 = "IF (Writing IS poor) THEN (Quality IS poor)"
+    R2 = "IF (Writing IS average) AND (Acting IS average) THEN (Quality IS average)"
+    R3 = "IF (Writing IS average) AND (Acting IS good) THEN (Quality IS average)"
+    R4 = "IF (Writing IS good) AND (Acting IS average) THEN (Quality IS average)"
+    R5 = "IF (Writing IS good) AND (Acting IS good) THEN (Quality IS good)"
+    R6 = "IF (Writing IS good) AND (Acting IS good) AND (Impact IS good) THEN (Quality IS amazing)"
+    fs.add_rules([R1, R2, R3, R4, R5, R6])
 
     # Set antecedents values
     fs.set_variable("Writing", writing_score)
